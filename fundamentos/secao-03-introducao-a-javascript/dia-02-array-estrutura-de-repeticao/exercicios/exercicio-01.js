44bf1905e322f58e7aec71cda2294b2471fb6fee
Priscilla Vieira <priscillavieira1994@gmail.com>
2023-07-06 11:17:27 -0300

    feito exercicio 01, e criei um comentario com algoritimo para verificacao do numero se ele e impar ou par.

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-estrutura-de-repeticao/exercicios/exercicio-01.js
@@ -0,0 +1,11 @@
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  // fiz um algoritimo para verificação do numero se ele é impar ou par
+  // if (numbers[index] % 2 === 0) {
+  //   console.log('É Par: ', numbers[index]);
+  // } else {
+  //   console.log('É ImPar: ', numbers[index]);
+  // }
+  console.log(numbers[index]);
+};
