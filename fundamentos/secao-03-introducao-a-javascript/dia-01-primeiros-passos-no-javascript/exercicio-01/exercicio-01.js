c01cfa93430477046584d452cdaf3ca269e87821
Priscilla Vieira <priscillavieira1994@gmail.com>
2023-07-05 21:44:44 -0300

    feitoo exercicio 01

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicio-01/exercicio-01.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 = 5;
+
+console.log(`A Soma de ${num1} + ${num2} é:`, num1 + num2); // utilizando template string
+console.log('A Subtração de ' + num1 + ' - ' + num2 + ' é:', num1 - num2); // utilizando concatenação
+console.log ('Multiplicação', num1 * num2);
+console.log( 'Divisão', num1 / num2);
+console.log('Módulo', num1 % num2);
