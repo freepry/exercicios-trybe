6d34998a17a00095d506d02389bcdf1221d1777c
Priscilla Vieira <priscillavieira1994@gmail.com>
2023-07-05 22:11:11 -0300

    feito exercicio 03

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicios/exercicio-02.js
@@ -0,0 +1,7 @@
+let num1 = 20;
+let num2 = 2;
+if (num1 < num2) {
+    console.log(`O maior é o num2: ${num2}`)
+} else {
+    console.log(`O maior é o num1: ${num1}`)
+}
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicios/exercicio-03.js
@@ -0,0 +1,23 @@
+let num1 = 5;
+let num2 = 20;
+let num3 = 23;
+
+if (num1 >= num2) {
+    if (num1 >= num3) {
+        console.log(`O maior é o num1: ${num1}`);
+    } else {
+        console.log(`O maior é o num3: ${num3}`);
+    }
+    } else {
+        if (num2 >= num3) {
+            console.log(`O maior é o num2: ${num2}`);
+        } else {
+            console.log(`O maior é o num3: ${num3}`);
+        }
+    }
+
+
+
+
+
+
